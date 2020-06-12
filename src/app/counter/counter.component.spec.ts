92aeb8964e2faf9b250b8742900955a985e5130f
William Marques <wmarques.pro@gmail.com>
2020-06-12 15:52:39 +0200

    other test

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,10 +1,15 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {CounterComponent} from './counter.component';
+import {By} from '@angular/platform-browser';
+import {HarnessLoader} from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import {MatButtonHarness} from '@angular/material/button/testing';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
   let fixture: ComponentFixture<CounterComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,16 +21,29 @@ describe('CounterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CounterComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
-  it('should increment', () => {
-    component.add();
-    expect(component.count).toBe(1);
+  it('should increment', async () => {
+    // GIVEN
+    const button = await loader.getHarness(MatButtonHarness.with({
+      text: '+',
+    }));
+
+    // WHEN
+    await button.click();
+
+    // THEN
+    expect(getCountValue()).toBe('1');
   });
 
   it('should decrement', () => {
     component.minus();
     expect(component.count).toBe(-1);
   });
+
+  function getCountValue() {
+    return fixture.debugElement.query(By.css('[data-testid="counter"]')).nativeElement.innerHTML;
+  }
 });
--- src/app/user-form/user-form.component.spec.ts
+++ src/app/user-form/user-form.component.spec.ts
@@ -5,10 +5,15 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldHarness} from '@angular/material/form-field/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import {HarnessLoader} from '@angular/cdk/testing';
+import {MatInputHarness} from '@angular/material/input/testing';
 
 describe('UserFormComponent', () => {
   let component: UserFormComponent;
   let fixture: ComponentFixture<UserFormComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,10 +26,21 @@ describe('UserFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserFormComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display invalid email when typing Cricri', async () => {
+    // GIVEN
+    const harness = await loader.getHarness(MatFormFieldHarness);
+    const input = await harness.getControl(MatInputHarness);
+
+    // WHEN
+    await input.setValue('invalid');
+    await input.blur();
+
+    // THEN
+    const errors = await harness.getTextErrors();
+    expect(errors[0]).toBe('Not a valid email');
   });
 });
